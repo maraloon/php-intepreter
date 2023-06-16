edd2a5c5815def4ffd7a386240c9e2805e20ad02
mara <mara@the-witch.ru>
2023-06-17 01:55:10 +0300

    Lexer

---
+++ src/lexer/lexer.rs
@@ -0,0 +1,269 @@
+use std::fmt::Display;
+
+#[derive(Debug, PartialEq)]
+pub enum Token {
+    Illegal(String),
+    Eof,
+    Ident(String),
+    Int(String),
+    Var(String),
+    Assign,
+    Plus,
+    Comma,
+    Semicolon,
+    Lparen,
+    Rparen,
+    Lbrace,
+    Rbrace,
+    LT,
+    // GT,
+    Function,
+    True,
+    False,
+    If,
+    Else,
+    Return,
+    StartTag, // <?php
+}
+
+impl Display for Token {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        return match self {
+            Token::Ident(x) => write!(f, "Ident({})", x),
+            Token::Var(x) => write!(f, "Var({})", x),
+            Token::Int(x) => write!(f, "Int({})", x),
+            Token::Illegal(e) => write!(f, "Illegal({})", e),
+            Token::Eof => write!(f, "Eof"),
+            Token::Assign => write!(f, "Assign"),
+            Token::Plus => write!(f, "Plus"),
+            Token::Comma => write!(f, "Comma"),
+            Token::Semicolon => write!(f, "Semicolon"),
+            Token::Lparen => write!(f, "Lparen"),
+            Token::Rparen => write!(f, "Rparen"),
+            Token::Lbrace => write!(f, "Lbrace"),
+            Token::Rbrace => write!(f, "Rbrace"),
+            Token::LT => write!(f, "LT"),
+            // Token::GT => write!(f, "GT"),
+            Token::Function => write!(f, "Function"),
+            Token::True => write!(f, "True"),
+            Token::False => write!(f, "False"),
+            Token::If => write!(f, "If"),
+            Token::Else => write!(f, "Else"),
+            Token::Return => write!(f, "Return"),
+            Token::StartTag => write!(f, "StartTag"),
+        };
+    }
+}
+
+pub struct Lexer {
+    input: Vec<u8>,
+    position: usize,
+    read_position: usize,
+    ch: u8,
+}
+
+impl Lexer {
+    pub fn new(input: String) -> Lexer {
+        let mut l = Lexer {
+            input: input.into_bytes(),
+            position: 0,
+            read_position: 0,
+            ch: 0,
+        };
+        l.read_char();
+        return l;
+    }
+
+    fn read_char(&mut self) {
+        if self.read_position >= self.input.len() {
+            self.ch = 0;
+        } else {
+            self.ch = self.input[self.read_position];
+        }
+        self.position = self.read_position;
+        self.read_position = self.read_position + 1;
+    }
+
+    fn skip_whitespaces(&mut self) {
+        while self.ch.is_ascii_whitespace() {
+            self.read_char();
+        }
+    }
+
+    pub fn next_token(&mut self) -> Result<Token, String> {
+        self.skip_whitespaces();
+
+        let token = match self.ch {
+            b'=' => Token::Assign,
+            b'+' => Token::Plus,
+            b',' => Token::Comma,
+            b';' => Token::Semicolon,
+            b'(' => Token::Lparen,
+            b')' => Token::Rparen,
+            b'{' => Token::Lbrace,
+            b'}' => Token::Rbrace,
+            b'<' => {
+                if self.peek() == b'?' {
+                    self.read_char();
+                    self.read_char();
+                    let literal = self.read_identifier();
+                    return Ok(match literal.as_str() {
+                        "php" => Token::StartTag,
+                        _ => Token::Illegal(String::from("False symbols after <?")),
+                    });
+                } else {
+                    Token::LT
+                }
+            }
+            b'$' => {
+                self.read_char();
+                let var_name = self.read_var_name();
+                return Ok(match var_name {
+                    Ok(_) => Token::Var(var_name.unwrap()),
+                    Err(e) => Token::Illegal(e.to_string()),
+                });
+            }
+            b'a'..=b'z' | b'A'..=b'Z' | b'_' => {
+                let literal = self.read_identifier();
+                return Ok(match literal.as_str() {
+                    "function" => Token::Function,
+                    "true" => Token::True,
+                    "false" => Token::False,
+                    "if" => Token::If,
+                    "else" => Token::Else,
+                    "return" => Token::Return,
+                    _ => Token::Ident(literal),
+                });
+            }
+            b'0'..=b'9' => {
+                return Ok(Token::Int(self.read_int()));
+            }
+            0 => Token::Eof,
+            _ => unreachable!("if you see it - shame on you"),
+        };
+
+        self.read_char();
+        return Ok(token);
+    }
+
+    fn peek(&self) -> u8 {
+        if self.read_position >= self.input.len() {
+            return 0;
+        } else {
+            return self.input[self.read_position];
+        }
+    }
+
+    fn read_int(&mut self) -> String {
+        let position = self.position;
+        while self.ch.is_ascii_digit() {
+            self.read_char();
+        }
+        return String::from_utf8_lossy(&self.input[position..self.position]).to_string();
+    }
+
+    fn read_identifier(&mut self) -> String {
+        let position = self.position;
+        while self.ch.is_ascii_alphabetic() || self.ch == b'_' {
+            self.read_char();
+        }
+
+        return String::from_utf8_lossy(&self.input[position..self.position]).to_string();
+    }
+
+    fn read_var_name(&mut self) -> Result<String, &str> {
+        let position = self.position;
+
+        if self.ch.is_ascii_digit() {
+            return Err("First symbol of var can't be digit");
+        }
+
+        while self.ch.is_ascii_alphanumeric() || self.ch == b'_' {
+            self.read_char();
+        }
+
+        return Ok(String::from_utf8_lossy(&self.input[position..self.position]).to_string());
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{Lexer, Token};
+
+    #[test]
+    fn get_next_token() -> Result<(), String> {
+        let input = "<?php
+        $five = 5;
+        $ten = 10;
+
+        $add = function($x, $y) {
+            $x + $y;
+        };
+
+        $result = add($five, $ten);";
+
+        let tokens = vec![
+            Token::StartTag,
+            Token::Var(String::from("five")),
+            Token::Assign,
+            Token::Int(String::from("5")),
+            Token::Semicolon,
+            Token::Var(String::from("ten")),
+            Token::Assign,
+            Token::Int(String::from("10")),
+            Token::Semicolon,
+            Token::Var(String::from("add")),
+            Token::Assign,
+            Token::Function,
+            Token::Lparen,
+            Token::Var(String::from("x")),
+            Token::Comma,
+            Token::Var(String::from("y")),
+            Token::Rparen,
+            Token::Lbrace,
+            Token::Var(String::from("x")),
+            Token::Plus,
+            Token::Var(String::from("y")),
+            Token::Semicolon,
+            Token::Rbrace,
+            Token::Semicolon,
+            Token::Var(String::from("result")),
+            Token::Assign,
+            Token::Ident(String::from("add")),
+            Token::Lparen,
+            Token::Var(String::from("five")),
+            Token::Comma,
+            Token::Var(String::from("ten")),
+            Token::Rparen,
+            Token::Semicolon,
+            Token::Eof,
+        ];
+
+        let mut lexer = Lexer::new(input.into());
+        for expected_token in tokens {
+            let next_token = lexer.next_token()?;
+            println!("expected: {:?}, received {:?}", expected_token, next_token);
+            assert_eq!(expected_token, next_token);
+        }
+
+        return Ok(());
+    }
+
+    #[test]
+    fn false_var_name() -> Result<(), String> {
+        let input = "$1five";
+
+        let tokens = vec![
+            Token::Illegal(String::from("First symbol of var can't be digit")),
+        ];
+
+        let mut lexer = Lexer::new(input.into());
+        for expected_token in tokens {
+            let next_token = lexer.next_token()?;
+            println!("expected: {:?}, received {:?}", expected_token, next_token);
+            assert_eq!(expected_token, next_token);
+        }
+
+        return Ok(());
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+// todo delete this line
+#![allow(unused)]
+
+pub mod lexer;
+
+use std::io;
+
+use users::get_current_username;
+
 fn main() {
-    println!("Hello, world!");
+    match get_current_username() {
+        Some(uname) => println!("Running php REPL as {:?}", uname),
+        None => println!("The current user does not exist!"),
+    }
+
+    loop {
+        let mut buffer = String::new();
+        let stdin = io::stdin(); // We get `Stdin` here.
+        stdin.read_line(&mut buffer);
+        println!("{}", buffer);
+    }
 }
